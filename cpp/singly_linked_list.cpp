86e2027d06cc58bbb7695d816f3dd37751ce7315
Arseniy <arseniy15078@gmail.com>
2026-03-18 05:05:07 +0700

    Update singly_linked_list.cpp

--- cpp/singly_linked_list.cpp
+++ cpp/singly_linked_list.cpp
@@ -130,57 +130,116 @@ void showMenu() {
 
 int main() {
     // Инициализация списка
-    Node* head = nullptr;
-    int choice;
-    int value;
+    // Node* head = nullptr;
+    // int choice;
+    // int value;
 
     setlocale(LC_ALL, "Russian");
 
-    do {
-        showMenu();
-        if (!(cin >> choice)) {
-            cin.clear();
-            cin.ignore(32767, '\n');
-            cout << "Неверный ввод. Попробуйте снова." << endl;
-            continue;
-        }
+    // do {
+    //     showMenu();
+    //     if (!(cin >> choice)) {
+    //         cin.clear();
+    //         cin.ignore(32767, '\n');
+    //         cout << "Неверный ввод. Попробуйте снова." << endl;
+    //         continue;
+    //     }
 
-        switch (choice) {
-            case 1:
-                createList(head);
-                break;
-            case 2:
-                cout << "Введите число для добавления: ";
-                cin >> value;
-                addElement(head, value);
-                break;
-            case 3:
-                cout << "Введите число для удаления: ";
-                cin >> value;
-                removeElement(head, value);
-                break;
-            case 4:
-                cout << "Введите число для поиска: ";
-                cin >> value;
-                if (searchElement(head, value)) {
-                    cout << "Элемент " << value << " найден." << endl;
-                } else {
-                    cout << "Элемент " << value << " не найден." << endl;
-                }
-                break;
-            case 5:
-                printList(head);
-                break;
-            case 6:
-                cout << "Выход из программы..." << endl;
-                break;
-            default:
-                cout << "Неверный номер пункта меню." << endl;
-        }
+    //     switch (choice) {
+    //         case 1:
+    //             createList(head);
+    //             break;
+    //         case 2:
+    //             cout << "Введите число для добавления: ";
+    //             cin >> value;
+    //             addElement(head, value);
+    //             break;
+    //         case 3:
+    //             cout << "Введите число для удаления: ";
+    //             cin >> value;
+    //             removeElement(head, value);
+    //             break;
+    //         case 4:
+    //             cout << "Введите число для поиска: ";
+    //             cin >> value;
+    //             if (searchElement(head, value)) {
+    //                 cout << "Элемент " << value << " найден." << endl;
+    //             } else {
+    //                 cout << "Элемент " << value << " не найден." << endl;
+    //             }
+    //             break;
+    //         case 5:
+    //             printList(head);
+    //             break;
+    //         case 6:
+    //             cout << "Выход из программы..." << endl;
+    //             break;
+    //         default:
+    //             cout << "Неверный номер пункта меню." << endl;
+    //     }
 
-    } while (choice != 6);
+    // } while (choice != 6);
 
+
+
+// # 1 Task
+    Node* head = nullptr;
+    int value;
+    do
+    {
+        if (!(cin >> value)) {
+                cin.clear();
+                cin.ignore(32767, '\n');
+                break;;
+            }
+
+        addElement(head,value);
+    }while (true);
+
+    printList(head);
+    Node* current = head;
+    int count = 0;
+    while (current != nullptr){
+        if (current->data < 0 && current->data % 7 == 0)
+            count++;
+        current = current->next;
+    }
+    cout << count << endl;
     clearList(head);
 
     return 0;
-}
+
+
+
+//     Node* head = nullptr;
+//     int value;
+//     int arithmetic_mean;
+//     do
+//     {
+//         if (!(cin >> value)) {
+//                 cin.clear();
+//                 cin.ignore(32767, '\n');
+//                 break;;
+//             }
+
+//         Node* newNode = new Node(value);
+//         Node* current = head;
+//         int count = 0;
+//         int temp = 0;
+//         while (current->next != nullptr) {
+//             current = current->next;
+//             count++;
+//             temp += current->data;
+//         }
+//         // Вставка между current и current->next
+//         newNode->next = current->next;
+//         current->next = newNode;
+//         arithmetic_mean = (temp + value) / (count + 1);
+//     }while (true);
+
+//     printList(head);
+//     cout << arithmetic_mean << endl;
+//     clearList(head);
+
+//     return 0;
+// }
